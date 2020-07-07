49d0320a3910fb04b73abbb3bfd43d6eb621cc93
unknown <lequynhkma@gmail.com>
2020-07-07 13:34:21 +0700

    Recycler View

---
+++ app/src/main/java/com/example/notetakingapp/Note.java
@@ -0,0 +1,6 @@
+package com.example.notetakingapp;
+
+public class Note {
+    private String content;
+
+}
---
+++ app/src/main/java/com/example/notetakingapp/recyclerview/MainRecyclerView.java
@@ -0,0 +1,32 @@
+package com.example.notetakingapp.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.notetakingapp.R;
+
+public class MainRecyclerView extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+    MyRecyclerViewAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_recycler_view);
+        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"};
+        RecyclerView recyclerView = findViewById(R.id.rvNumbers);
+        int numberOfColumns = 2;
+        recyclerView.setLayoutManager(new GridLayoutManager(this, numberOfColumns));
+        adapter = new MyRecyclerViewAdapter(this, data);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.i("TAG", "You Click number" + adapter.getItem(position)+ ",which is at cell position"+ position);
+    }
+}
